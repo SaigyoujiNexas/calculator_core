1f3ae373d82364c2ec2698fab3c0e94e25d008e3
yuki <SaigyoujiNexas@gmail.com>
2025-05-25 22:42:20 +0800

    feat: 补充单元测试

--- src/lib.rs
+++ src/lib.rs
@@ -16,4 +16,16 @@ pub fn calculate(expression: &str) -> CalcResult<Decimal> {
 
 #[cfg(test)]
 mod tests {
+    use rust_decimal::Decimal;
+
+    use crate::calculate;
+
+    #[test]
+    fn test_calc() {
+        // 27 + 3 * 5 = 27 + 15 = 42
+        let expression = "(cos(0) * 3) ^ 3 + 3 * ( 7 - 2)";
+        let ans = calculate(expression).unwrap();
+        assert_eq!(ans, Decimal::from(42))
+        
+    }
 }
